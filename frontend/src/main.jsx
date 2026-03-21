08f1856f6d9fb5a4811aea10d406bcbecc455a78
nnson128 <sonvipkl04@gmail.com>
2026-03-21 11:07:38 +0700

    abcd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { I18nProvider } from './i18n';
-import store from './store';
-import App from './App';
-import './index.css';
-import './styles/index.css';
-import { ConfigProvider } from 'antd';
-import antdTheme from './config/antdTheme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { I18nProvider } from "./i18n";
+import store from "./store";
+import App from "./App";
+import "./index.css";
+import "./styles/index.css";
+import { ConfigProvider } from "antd";
+import antdTheme from "./config/antdTheme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,11 +21,13 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter
+          future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+        >
           <I18nProvider>
             <ConfigProvider theme={antdTheme}>
               <App />
@@ -34,5 +36,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
